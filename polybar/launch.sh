3f6fdb8598295d6239df65276969d21e830f9f8e
Pedro Augusto <pedroaa57@gmail.com>
2021-10-09 02:30:00 -0300

    Update Files

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,6 +6,6 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar example 2>&1 | tee -a /tmp/polybar.log & disown
+polybar -c ~/.config/polybar/config.ini mybar 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
